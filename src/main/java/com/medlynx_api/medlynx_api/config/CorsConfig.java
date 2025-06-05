fb7fd85c8852bc0ecbd12144bc9e8cc2ab7ed077
Aleicer <aleicer.vesga@gmail.com>
2025-06-04 21:15:16 -0500

    Agregue cors para filtrar peticiones de origenes especificos

---
+++ src/main/java/com/medlynx_api/medlynx_api/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.medlynx_api.medlynx_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica a todos los endpoints
+                        .allowedOrigins(
+                                "http://localhost:3000",         // Frontend local
+                                "https://medlynx.vercel.app"     // Producción en Vercel (ajústalo según tu dominio real)
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
